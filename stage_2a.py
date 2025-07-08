96c2b6d7541cb4022e944f375689e353d18528bc
vkverma9534 <vkverma9534@gmail.com>
2025-07-08 11:59:22 +0530

    stage_2a.py

--- stage_2a.py
+++ stage_2a.py
@@ -1,18 +1,19 @@
-import pandas as pd
+def run():
+    import pandas as pd
+    import sqlite3
 
-stocks_df = pd.read_csv("inventory_clean.csv")
-sales_df = pd.read_csv("sales_clean.csv")
-returns_df = pd.read_csv("returns_clean.csv")
+    stocks_df = pd.read_csv("inventory_clean.csv")
+    sales_df = pd.read_csv("sales_clean.csv")
+    returns_df = pd.read_csv("returns_clean.csv")
 
+    conn = sqlite3.connect("mydb.db")
+    stocks_df.to_sql("INVENTORY", conn, if_exists="replace", index=False)
+    conn.close()
 
-conn = sqlite3.connect("mydb.db")
-stocks_df.to_sql("INVENTORY", conn, if_exists="replace", index=False)
-conn.close()
+    conn = sqlite3.connect("mydb.db")
+    sales_df.to_sql("SALES", conn, if_exists="replace", index=False)
+    conn.close()
 
-conn = sqlite3.connect("mydb.db")
-sales_df.to_sql("SALES", conn, if_exists="replace", index=False)
-conn.close()
-
-conn = sqlite3.connect("mydb.db")
-returns_df.to_sql("RETURNS", conn, if_exists="replace", index=False)
-conn.close()
+    conn = sqlite3.connect("mydb.db")
+    returns_df.to_sql("RETURNS", conn, if_exists="replace", index=False)
+    conn.close()
